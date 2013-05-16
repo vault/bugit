a466cfb6248b134b27ddf9b02a22b1e7e5291154
Michael Abed <mgabed@bu.edu>
2013-05-16 07:17:57 -0400

    Merge repo_new repo_add

--- repo_manage/urls.py
+++ repo_manage/urls.py
@@ -1,13 +1,11 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('repo_manage.views',
     url(r'^$', 'index', name='index'),
     url(r'^(?P<user_name>\w+)/$',  'repo_list', name='repo_list'),
-    url(r'^(?P<user_name>\w+)/create/$', 'repo_add', name='repo_add'),
+    url(r'^(?P<user_name>\w+)/create/$', 'repo_new', name='repo_new'),
     url(r'^(?P<user_name>[-\w]+)/(?P<repo_name>[-\w]+)/$', 'repo_view', name='repo_view'),
     url(r'^(?P<user_name>[-\w]+)/(?P<repo_name>[-\w]+)/delete/$', 'repo_delete', name='repo_delete'),
     url(r'^(?P<user_name>[-\w]+)/(?P<repo_name>[-\w]+)/settings/$', 'repo_edit', name='repo_edit'),
-    url(r'^(?P<user_name>[-\w]+)/settings/$', 'repo_new', name='repo_new'),
-
 )
 
