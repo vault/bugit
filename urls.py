3ca2f8c87a85f7a387505e28bb968214a81f5e7f
Michael Abed <mgabed@bu.edu>
2013-05-16 10:52:11 -0400

    Root page

--- urls.py
+++ urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template':'welcome.html'}),
     url(r'^repo/', include('repo_manage.urls')),
     url(r'^settings/', include('user_manage.urls')),
     url(r'^view/', include('viewer.urls')),
