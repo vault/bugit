e5102440c1ab33dc917f96f09ff684ebc531aa20
Michael Abed <mgabed@bu.edu>
2013-05-16 10:23:32 -0400

    Imrpove user management app

--- user_manage/views.py
+++ user_manage/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
-from django.contrib.auth.models import User
-from django.shortcuts import *
+from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.forms.models import model_to_dict
+from django.forms.util import ErrorList
+from django.template import RequestContext
+from django.db import IntegrityError
 
 from common.models import PublicKey 
-from common.util import *
+from common.util import get_context
 
 from user_manage.forms import UserForm, PublicKeyForm
 
@@ -40,13 +42,13 @@ def pubkey_add(request):
     if form.is_valid():
         key = form.save(commit=False)
         key.owner = user
-        key.save()
-        return redirect('user_settings')
-    else:
-        pubkeys = user.publickey_set.all()
-        user_form = UserForm(model_to_dict(user))
-        context = get_context(request, {'form' : form})
-        return render_to_response('user_manage/key_edit.html', context, context_instance=RequestContext(request))
+        try:
+            key.save()
+            return redirect('user_settings')
+        except IntegrityError:
+            form._errors["description"] = ErrorList(["You have a public key with that name already"])
+    context = get_context(request, {'form' : form})
+    return render_to_response('user_manage/key_edit.html', context, context_instance=RequestContext(request))
 
 
 def pubkey_delete(request, key_id):
@@ -61,15 +63,24 @@ def pubkey_edit(request, key_id=None):
         pk = get_object_or_404(PublicKey, pk=key_id)
     else:
         pk = None
+
     user = request.user
     if not user.is_authenticated() or pk.owner != user:
         return HttpResponse("Not allowed", status=401)
 
     if request.method == 'POST':
-        form = PublicKeyForm(request.POST, instance=pk)
+        if key_id is not None:
+            form = PublicKeyForm(request.POST, instance=pk)
+        else:
+            form = PublicKeyForm(request.POST)
         if form.is_valid():
-            pk = form.save()
-            return redirect('user_settings')
+            try:
+                pk = form.save()
+                return redirect('user_settings')
+            except IntegrityError:
+                form._errors["description"] = ErrorList(["You have a public key with that name already"])
+                context = get_context(request, {'form' : form, 'pk': pk})
+                return render_to_response('user_manage/key_edit.html', context, context_instance=RequestContext(request))
     elif request.method == 'GET':
         form = PublicKeyForm(model_to_dict(pk))
     else:
