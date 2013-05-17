08b609557c13f2c5dde89565b44be951737996c8
Michael Abed <mgabed@bu.edu>
2013-05-17 01:00:29 -0400

    TYPOS

--- viewer/urls.py
+++ viewer/urls.py
@@ -4,8 +4,8 @@ urlpatterns = patterns('viewer.views',
     url(r'^$', 'view_index'),
     url(r'^(?P<user_name>[-\w]+)/$', 'user_index'),
     url(r'^(?P<user_name>[-\w]+)/(?P<repo_name>[-\w]+)/$', 'repo_browse', name='repo_browse'),
-    url(r'^(?P<user_name>[-\w]+)/(?P<repo_name>[-\w]+)/plain/($?P<path>.*)$', 'repo_plain', name='repo_plain'),
-    url(r'^(?P<user_name>[-\w]+)/(?P<repo_name>[-\w]+)/snapshot/($?<path.*)$', 'repo_snapshot', name='repo_snapshot'),
+    url(r'^(?P<user_name>[-\w]+)/(?P<repo_name>[-\w]+)/plain/(?P<path>.*)$', 'repo_plain', name='repo_plain'),
+    url(r'^(?P<user_name>[-\w]+)/(?P<repo_name>[-\w]+)/snapshot/(?P<path>.*)$', 'repo_snapshot', name='repo_snapshot'),
     url(r'^(?P<user_name>[-\w]+)/(?P<repo_name>[-\w]+)/(?P<path>.*)$', 'repo_browse', name='repo_browse'),
 )
 
